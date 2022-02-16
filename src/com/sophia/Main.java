7e5377bee7d6567e5325314b1e93e4ad53e45d1e
yitong86 <sophiajiang86@hotmail.com>
2022-02-16 11:20:49 -0500

    Initial Commit

---
+++ src/com/sophia/Main.java
@@ -0,0 +1,26 @@
+package com.sophia;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args ) throws FileNotFoundException {
+	Scanner scanner = new Scanner(new File("C:/Users/sophi/Documents/words_alpha.txt"));
+
+    List<String> words = new ArrayList<>();
+
+
+    while (scanner.hasNext()){
+        words.add(scanner.nextLine());
+    }
+    Random rand = new Random();
+
+    String word = words.get(rand.nextInt(words.size()));
+        System.out.println(word);
+    }
+}
